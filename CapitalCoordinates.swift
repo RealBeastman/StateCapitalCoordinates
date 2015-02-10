9bca5671b5dd18a2706aee20332a9ae3bad596eb
Joshua Eastman <joshuabeastman@gmail.com>
2015-02-09 18:26:05 -0700

    ignore

--- CapitalCoordinates.swift
+++ CapitalCoordinates.swift
@@ -18,8 +18,8 @@ public class CapitalCoordinates {
     
     class func coordinates() -> [String : CLLocationCoordinate2D] {
         return ["Colorado":CLLocationCoordinate2DMake(39.7618, 104.8811),
-            "Texas": CLLocationCoordinate2DMake(30.2500, 97.75000)
-        ]
+                "Texas": CLLocationCoordinate2DMake(30.2500, 97.75000)
+                ]
     }
     
 }
