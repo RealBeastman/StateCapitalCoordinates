4f589a2eaaaf960f2cc4517ee224d67aee91efbe
Joshua Eastman <joshuabeastman@gmail.com>
2015-02-09 18:25:53 -0700

    Creats dictionary and return values for states

--- CapitalCoordinates.swift
+++ CapitalCoordinates.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+import CoreLocation
+
+public class CapitalCoordinates {
+    
+    class func coordinatesFor(state: String) -> CLLocationCoordinate2D {
+        var coords = coordinates()
+        return coords[state]!
+    }
+    
+    class func coordinates() -> [String : CLLocationCoordinate2D] {
+        return ["Colorado":CLLocationCoordinate2DMake(39.7618, 104.8811),
+            "Texas": CLLocationCoordinate2DMake(30.2500, 97.75000)
+        ]
+    }
+    
+}
+
+CapitalCoordinates.coordinatesFor("Texas")
+
+
+println("TEST")
+
+
+
+
+
